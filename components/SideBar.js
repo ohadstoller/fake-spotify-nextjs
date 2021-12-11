a74a431ed2ff4df075f03f5b15feaadc7c56a40b
Ohad Stoller <ohadstoller@Ohads-MacBook-Pro.local>
2021-12-11 21:10:30 +0200

    adding session provider wrapper

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {HomeIcon, PlusCircleIcon, LibraryIcon, SearchIcon, HeartIcon, RssIcon} from "@heroicons/react/outline";
-import {signOut} from "next-auth/react";
+import {signOut, useSession} from "next-auth/react";
 
 const SideBar = () => {
+    const {data: session, status} = useSession()
+    console.log('session/: ',session);
     return (
         <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
             <div className="space-y-4">
