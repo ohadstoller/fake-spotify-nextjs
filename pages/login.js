c0af777322ef15afc721baf116741963f8e3c9b8
Ohad Stoller <ohadstoller@Ohads-MacBook-Pro.local>
2021-12-11 22:25:46 +0200

    redesigt login page

--- pages/login.js
+++ pages/login.js
@@ -1,39 +1,36 @@
-import React from 'react';
 import {getProviders, signIn} from "next-auth/react";
 
-const Login = ({providers}) => {
+function Login({providers}) {
     return (
-        <div>
-            <img className="w-52 mb-5" src="https://tinyurl.com/2p8vwsd4" alt=""/>
+        <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+            <img
+                className="w-52 mb-4"
+                src="https://tinyurl.com/2p98n3vn"
+                alt="fake spotify"
+            />
 
             {Object.values(providers).map((provider) => (
                 <div key={provider.name}>
                     <button
-                        onClick={
-                            () => {
-                                signIn(provider.id, {callbackUrl: "/"})
-                            }
-                        }
-
+                        onClick={() => signIn(provider.id, {callbackUrl: "/"})}
+                        className="bg-[#35B300] text-black font-bold uppercase p-2 rounded-full"
                     >
-                        login with {provider.name}
+                        Login with {provider.name}
                     </button>
                 </div>
-
-            ))
-            }
+            ))}
         </div>
     );
-};
+}
 
 export default Login;
 
 export async function getServerSideProps() {
-    const providers = await getProviders()
+    const providers = await getProviders();
+
     return {
         props: {
-            providers
-        }
-    }
-
+            providers,
+        },
+    };
 }
