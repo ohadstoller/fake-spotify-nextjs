c539f24dadd439986179030c335f0c3f6da14b88
Ohad Stoller <ohadstoller@Ohads-MacBook-Pro.local>
2021-12-13 18:22:07 +0200

    adding a default playlist for when first rendering for dev

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -1,8 +1,31 @@
 import {atom} from "recoil";
 
+const defaultPlaylist = {
+    collaborative: false,
+    description: "",
+    external_urls: {spotify: 'https://open.spotify.com/playlist/1ULrWII99j9006T69KjblM'},
+    href: "https://api.spotify.com/v1/playlists/1ULrWII99j9006T69KjblM",
+    id: "1ULrWII99j9006T69KjblM",
+    images: [{
+        height: 640,
+        url: "https://i.scdn.co/image/ab67616d0000b273052bd0ef86fa73c77263bfbe",
+        width: 640,
+    }],
+    name: "Tim Maia 1970",
+    primary_color: null,
+    public: true,
+    snapshot_id: "MiwzODIyYjBiOWM5ODJlZWFhNzgyYjBkMDQwODViY2NmYzFmMGI4N2Uz",
+    tracks: {
+        href: 'https://api.spotify.com/v1/playlists/1ULrWII99j9006T69KjblM/tracks',
+        total: 12
+    },
+    type: "playlist",
+    uri: "spotify:playlist:1ULrWII99j9006T6"
+}
+
 export const playlistState = atom({
     key: 'playlistState',
-    default: null
+    default: defaultPlaylist
 })
 
 export const playlistIdState = atom({
