4a1e7b082bc02ba85fe5287436025233f114ce3e
Ohad Stoller <ohadstoller@Ohads-MacBook-Pro.local>
2021-12-15 14:45:50 +0200

    adding recoil song state, and ui for song comp

--- components/Songs.js
+++ components/Songs.js
@@ -1,15 +1,13 @@
-import {useRecoilValue} from "recoil";
-import {playlistState} from "@/atoms/playlistAtom";
-import {Song} from "@/components/Song";
-
+import { useRecoilValue } from "recoil";
+import { playlistState } from "@/atoms/playlistAtom";
+import Song from "./Song";
 
 function Songs() {
     const playlist = useRecoilValue(playlistState);
-    console.log(playlist)
     return (
         <div className="text-white px-8 flex flex-col space-y-8 pb-28">
-            {playlist?.tracks.items.map((track) => (
-                <Song track={track}/>
+            {playlist?.tracks.items.map((track, index) => (
+                <Song key={track.track.id} track={track} order={index} />
             ))}
         </div>
     );
